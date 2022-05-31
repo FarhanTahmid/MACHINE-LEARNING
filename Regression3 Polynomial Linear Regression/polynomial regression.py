e511021ed86668d2f7f4e339110d39b05fd3c22b
Farhan Tahmid <farhantahmid881@gmail.com>
2022-05-31 22:35:32 +0600

    polynomial linear model

--- Regression3 Polynomial Linear Regression/polynomial regression.py
+++ Regression3 Polynomial Linear Regression/polynomial regression.py
@@ -16,4 +16,33 @@ not needed here as our dataset is too small
 
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
 '''
+
+#fitting linear regression in dataset (building two models just to compare. no need to create this)
+from sklearn.linear_model import LinearRegression
+linearRegressor=LinearRegression()
+linearRegressor.fit(x, y)
+
+#fitting polynomial regression in dataset
+#polynomial regression model
+from sklearn.preprocessing import PolynomialFeatures
+polynomialRegressor=PolynomialFeatures(degree=2)
+xPolynomial=polynomialRegressor.fit_transform(x)
+linearRegressor2=LinearRegression()  #making this linear regression object to fit our polynomial x in to the linear regressor model
+linearRegressor2.fit(xPolynomial, y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
