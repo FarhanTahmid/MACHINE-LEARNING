71b6ec67fa3c3a9c506904661207d91537ff79ec
Farhan Tahmid <farhantahmid881@gmail.com>
2022-06-03 12:22:56 +0600

    SVR

---
+++ Regression4 Support Vector Regression/support_vector_regression.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  3 10:42:42 2022
+
+@author: farha
+"""
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+x = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+'''
+not needed here as our dataset is too small
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
+'''
+
+#fitting the SVR in dataset
+from sklearn.svm import SVR
+regressor=SVR(kernel='rbf')
+regressor.fit(x,y)
+
+
+#predicting the result with regression
+yPrediction=regressor.predict([[6.5]])
+
+
+#Visulaizing the Support vector regression results
+plt.scatter(x, y,color='red')
+plt.plot(x,regressor.predict(x),color='blue')
+plt.title("Truth or Bluff (SVR model)")
+plt.xlabel("Position level")
+plt.ylabel("Salary")
+plt.show()
